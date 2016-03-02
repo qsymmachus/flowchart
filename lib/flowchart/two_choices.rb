9735e6d1fa680127043e4ff93edfc1550b5e0622
John Olmsted <john@olmsted.io>
2016-03-02 11:17:01 -0800

    Refactor Flowchart::TwoChoices

--- lib/flowchart/two_choices.rb
+++ lib/flowchart/two_choices.rb
@@ -1,25 +1,19 @@
 module Flowchart
   class TwoChoices < Choice
-    def initialize(first_choice, second_choice)
-      @first_choice = first_choice
-      @second_choice = second_choice
-    end
-
-    def choose_first(data)
-      raise NotImplementedError
-    end
-
-    def choose_second(data)
-      raise NotImplementedError
+    def initialize(args)
+      @first_choice = args.fetch(:first_choice)
+      @second_choice = args.fetch(:second_choice)
+      @choose_first = args.fetch(:choose_first)
+      @choose_second = args.fetch(:choose_second)
     end
 
     def decide(data = nil)
-      if choose_first(data)
+      if @choose_first.call(data)
         @first_choice.decide(data)
-      elsif choose_second(data)
+      elsif @choose_second.call(data)
         @second_choice.decide(data)
       else
-        Flowchart::NoChoice.new
+        Flowchart::NoDecision.new
       end
     end
   end
--- spec/two_choices_spec.rb
+++ spec/two_choices_spec.rb
@@ -1,15 +1,75 @@
 require 'spec_helper'
 
 describe Flowchart::TwoChoices do
-  before do
-    @first_choice = Flowchart::Choice.new
-    @second_choice = Flowchart::Choice.new
-    @choices = Flowchart::TwoChoices.new(@first_choice, @second_choice)
+  describe 'instantiation' do
+    it 'throws an error if expected argument key is missing' do
+      @expected_keys = [:first_choice, :second_choice, :choose_first, :choose_second]
+
+      @expected_keys.each do |excluded_key|
+        expect {
+          # Build an args hash with all but one of the expected keys:
+          args = Hash.new(
+            @expected_keys - [excluded_key].zip(
+              (1...@expected_keys.length - 1).to_a
+            )
+          )
+          Flowchart::TwoChoices.new(args)
+        }.to raise_error KeyError
+      end
+    end
   end
 
   describe 'decide' do
-    it 'raises a NotImplementedError' do
-      expect { @choices.decide }.to raise_error(NotImplementedError)
+    before do
+      @first_decision = Flowchart::Decision.new('first')
+      @second_decision = Flowchart::Decision.new('second')
+      @first_choice = double(Flowchart::Choice::class, decide: @first_decision)
+      @second_choice = double(Flowchart::Choice::class, decide: @second_decision)
+    end
+
+    context 'choose_first returns true' do
+      it 'returns the result of first_choice#decide' do
+        @choose_first = Proc.new { |data| true }
+        @choose_second = Proc.new { |data| false }
+        @two_choices = Flowchart::TwoChoices.new({
+          first_choice: @first_choice,
+          second_choice: @second_choice,
+          choose_first: @choose_first,
+          choose_second: @choose_second
+        })
+
+        expect(@two_choices.decide).to equal @first_choice.decide
+      end
+    end
+
+    context 'choose_second returns true' do
+      it 'returns the result of second_choice#decide' do
+        @choose_first = Proc.new { |data| false }
+        @choose_second = Proc.new { |data| true }
+        @two_choices = Flowchart::TwoChoices.new({
+          first_choice: @first_choice,
+          second_choice: @second_choice,
+          choose_first: @choose_first,
+          choose_second: @choose_second
+        })
+
+        expect(@two_choices.decide).to equal @second_choice.decide
+      end
+    end
+
+    context 'neither choose_first nor choose_second return true' do
+      it 'returns an instance of Flowchart::NoChoice' do
+        @choose_first = Proc.new { |data| false }
+        @choose_second = Proc.new { |data| false }
+        @two_choices = Flowchart::TwoChoices.new({
+          first_choice: @first_choice,
+          second_choice: @second_choice,
+          choose_first: @choose_first,
+          choose_second: @choose_second
+        })
+
+        expect(@two_choices.decide).to be_a Flowchart::NoDecision
+      end
     end
   end
 end
